5592b160ce16e9df55d7b64022fe33197086cbf4
Ivan Obeso Agüera <ivan.obeso1@gmail.com>
2016-12-29 16:03:11 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Catastro></Catastro>
+  </div>
+</template>
+
+<script>
+import Catastro from './components/Catastro';
+
+export default {
+  name: 'app',
+  components: {
+    Catastro,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Catastro.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="catastro">
+    <h1>Hola amigos {{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'catastro',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
