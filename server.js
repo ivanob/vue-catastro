21855a6ab856ba08b97a2178b4f80c790eb60651
Ivan Obeso Agüera <ivan.obeso1@gmail.com>
2017-01-01 03:24:56 +0100

    added some modifications to use bootstrap

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var serveStatic = require('serve-static')
 
 var app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 var port = process.env.PORT || 5000
 app.listen(port)
--- src/components/Catastro.vue
+++ src/components/Catastro.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="catastro">
     <h1>Hola amigos {{msg}}</h1>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <div class="jumbotron">
+  <h1>Bootstrap Tutorial</h1>
+  <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive,
+  mobile-first projects on the web.</p>
+</div>
+<div class="container">
+  <p>This is some text.</p>
+  <p>This is another text.</p>
+</div>
   </div>
 </template>
 
