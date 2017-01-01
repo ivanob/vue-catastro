09290aa2f6da41cccc4ad80478f979f2e868fdfd
Ivan Obeso Agüera <ivan.obeso1@gmail.com>
2017-01-01 03:30:00 +0100

    added some modifications to use bootstrap

--- src/components/Catastro.vue
+++ src/components/Catastro.vue
@@ -1,16 +1,45 @@
 <template>
   <div class="catastro">
-    <h1>Hola amigos {{msg}}</h1>
-    <button type="button" class="btn btn-primary">Primary</button>
-    <div class="jumbotron">
-  <h1>Bootstrap Tutorial</h1>
-  <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive,
-  mobile-first projects on the web.</p>
+    <div class="page-header text-center">
+   <h1>Lugar de Quintanilla </h1>
+
+ </div>
+<div class="wrapper container">
+ <div class="modal-body row">
+   <div class="col-md-6 text-right">
+     <!-- Your first column here -->
+     <img src="../assets/pic1_pueblo1.jpg" class="img-thumbnail" alt="Cinque Terre" width="404" height="336">
+   </div>
+   <div class="col-md-6">
+     <!-- Your second column here -->
+     <script id="nuestra-plantilla" type="text/x-handlebars-template">
+       <p>Hola {{usuario}}</p>
+     </script>
+   </div>
+ </div>
 </div>
-<div class="container">
-  <p>This is some text.</p>
-  <p>This is another text.</p>
-</div>
+
+ <div class="text-center">
+ <nav aria-label="Page navigation">
+ <ul class="pagination">
+ <li>
+   <a href="#" aria-label="Previous">
+     <span aria-hidden="true">&laquo;</span>
+   </a>
+ </li>
+ <li><a href="#">1</a></li>
+ <li><a href="#">2</a></li>
+ <li><a href="#">3</a></li>
+ <li><a href="#">4</a></li>
+ <li><a href="#">5</a></li>
+ <li>
+   <a href="#" aria-label="Next">
+     <span aria-hidden="true">&raquo;</span>
+   </a>
+ </li>
+ </ul>
+ </nav>
+ </div>
   </div>
 </template>
 
