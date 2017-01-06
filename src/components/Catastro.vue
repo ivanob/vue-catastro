eeeb21bad0bfb840c71b562ef78b7c4e1b37af7f
Ivan Obeso Agüera <ivan.obeso1@gmail.com>
2017-01-06 00:42:58 +0000

    pagination working

--- src/components/Catastro.vue
+++ src/components/Catastro.vue
@@ -17,31 +17,12 @@
  </div>
 </div>
 
- <div class="text-center">
-   <pagination :current-page="pageOne.currentPage"
-                 :total-pages="pageOne.totalPages"
-                 @page-changed="pageOneChanged">
-     </pagination>
- <!--<nav aria-label="Page navigation">
- <ul class="pagination">
- <li>
-   <a href="#" aria-label="Previous">
-     <span aria-hidden="true">&laquo;</span>
-   </a>
- </li>
- <li><a href="#">1</a></li>
- <li><a href="#">2</a></li>
- <li><a href="#">3</a></li>
- <li><a href="#">4</a></li>
- <li><a href="#">5</a></li>
- <li>
-   <a href="#" aria-label="Next">
-     <span aria-hidden="true">&raquo;</span>
-   </a>
- </li>
- </ul>
- </nav>-->
- </div>
+   <div class="text-center">
+     <pagination :current-page="pageOne.currentPage"
+                   :total-pages="pageOne.totalPages"
+                   @page-changed="pageOneChanged">
+       </pagination>
+   </div>
   </div>
 </template>
 
@@ -55,14 +36,14 @@ export default {
   data() {
         return {
             pageOne: {
-                currentPage: 1,
-                totalPages: 10
+                currentPage: 5,
+                totalPages: 5
             }
         }
     },
 
     methods: {
-        pageOneChanged (pageNum) {
+        pageOneChanged: function (pageNum) {
             this.pageOne.currentPage = pageNum
         }
     }
--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -17,9 +17,68 @@
 </template>
 
 <script>
+import Util from '../services/Util'
 
 export default {
-  name: 'Pagination'/*,
+  props: {
+    // Current Page
+    currentPage: {
+      type: Number,
+      required: true
+    },
+    // Total page
+    totalPages: Number,
+    // Items per page
+    itemsPerPage: Number,
+    // Total items
+    totalItems: Number,
+    // Visible Pages
+    visiblePages: {
+      type: Number,
+      default: 5,
+      coerce: (val) => parseInt(val)
+    }
+  },
+  data () {
+    return {}
+  },
+  name: 'Pagination',
+  computed: {
+    lastPage: function () {
+      if (this.totalPages) {
+        return this.totalPages
+      } else {
+        return this.totalItems % this.itemsPerPage === 0
+          ? this.totalItems / this.itemsPerPage
+          : Math.floor(this.totalItems / this.itemsPerPage) + 1
+      }
+    },
+    paginationRange: function () {
+      let start =
+        this.currentPage - this.visiblePages / 2 <= 0
+        ? 1 : this.currentPage + this.visiblePages / 2 > this.lastPage
+        ? Util.lowerBound(this.lastPage - this.visiblePages + 1, 1)
+        : Math.ceil(this.currentPage - this.visiblePages / 2)
+
+      let range = []
+
+      for (let i = 0; i < this.visiblePages && i < this.lastPage; i++) {
+        range.push(start + i)
+      }
+
+      return range
+    }
+  },
+  methods: {
+    activePage: function (pageNum) {
+      return this.currentPage === pageNum ? 'active' : ''
+    },
+    pageChanged: function (pageNum) {
+      this.$emit('page-changed', pageNum)
+    }
+  }
+
+/*,
   props:['nombre','page1']
   ready: function(){
     console.log("LLEGA")
