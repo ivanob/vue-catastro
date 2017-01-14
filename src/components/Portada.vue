3fc7c49b8d202c7af99cf6423358223a891d50f7
Ivan Obeso Agüera <ivan.obeso1@gmail.com>
2017-01-14 22:19:25 +0000

    reestructuration

---
+++ src/components/Portada.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="portada">
+    <h1>Esto es la portada</h1>
+    <div data-slick='{"slidesToShow": 4, "slidesToScroll": 4}'>
+      <div><h3>1</h3></div>
+      <div><h3>2</h3></div>
+      <div><h3>3</h3></div>
+      <div><h3>4</h3></div>
+      <div><h3>5</h3></div>
+      <div><h3>6</h3></div>
+    </div>
+  </div>
+</template>
+
+<script>
+require('slick-carousel')
+
+export default {
+  name: 'portada',
+  components : {  },
+  props:[]
+}
+</script>
+
+<style>
+
+</style>
