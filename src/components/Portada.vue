0ff49425a742047ccff9b3a509a7fd27ace33e94
Ivan Obeso Agüera <ivan.obeso1@gmail.com>
2017-01-15 00:42:30 +0000

    added carousel

--- src/components/Portada.vue
+++ src/components/Portada.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="portada">
+    <!-- JUMBOTRON -->
     <div class="jumbotron">
       <h1>Valle de La Masón</h1>
       <h2>En el catastro del Marqués de la Ensenada</h2>
@@ -8,6 +9,62 @@
         <a class="btn btn-primary btn-lg" href="#" role="button">Preguntas del catastro</a>
       </p>
     </div>
+
+    <!-- CAROUSEL -->
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+      <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+      <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+      <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+      <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+      <li data-target="#carousel-example-generic" data-slide-to="5"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="http://placehold.it/1200x315" alt="...">
+        <div class="carousel-caption">
+        	<h3>Lugar de Cires</h3>
+        </div>
+      </div>
+      <div class="item">
+        <img src="http://placehold.it/1200x315" alt="...">
+        <div class="carousel-caption">
+        	<h3>Lugar de La Fuente</h3>
+        </div>
+      </div>
+      <div class="item">
+        <img src="http://placehold.it/1200x315" alt="...">
+        <div class="carousel-caption">
+        	<h3>Lugar de Quintanilla</h3>
+        </div>
+      </div>
+      <div class="item">
+        <img src="http://placehold.it/1200x315" alt="...">
+        <div class="carousel-caption">
+        	<h3>Lugar de Rio</h3>
+        </div>
+      </div>
+      <div class="item">
+        <img src="http://placehold.it/1200x315" alt="...">
+        <div class="carousel-caption">
+        	<h3>Lugar de Sobrepeña</h3>
+        </div>
+      </div>
+    </div>
+
+      <!-- Controls -->
+      <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+      </a>
+      <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+      </a>
+    </div> <!-- Carousel -->
+
   </div>
 </template>
 
@@ -20,9 +77,12 @@ export default {
 </script>
 
 <style>
-.jumbotron{
+.jumbotron {
   margin: 25px;
   padding: 25px;
+}
+#carousel-example-generic {
+  margin: 25px;
 }
 
 </style>
