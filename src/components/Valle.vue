f733a40a534861ab0fa67a4e8ca6dba6a1024b2f
Ivan Obeso <ivan.obeso1@gmail.com>
2017-09-24 13:57:49 +0100

    addded the new vue gallery

--- src/components/Valle.vue
+++ src/components/Valle.vue
@@ -1,5 +1,45 @@
 <template>
   <div class="valle">
-    
+    <div>
+    <gallery :images="images" :index="index" @close="index = null"></gallery>
+    <div
+      class="image"
+      v-for="image, imageIndex in images"
+      @click="index = imageIndex"
+      :style="styleObject(image)"
+    ></div>
+  </div>
   </div>
 </template>
+
+<script>
+  import VueGallery from 'vue-gallery';
+
+  export default {
+    data: function () {
+      return {
+        images: [
+          'https://dummyimage.com/800/ffffff/000000',
+          'https://dummyimage.com/1600/ffffff/000000',
+          'https://dummyimage.com/1280/000000/ffffff',
+          'https://dummyimage.com/400/000000/ffffff',
+        ],
+        index: null,
+
+      };
+    },
+    methods: {
+      styleObject: function(image){
+        return {
+          backgroundImage: 'url(' + image + ')',
+           width: '50px',
+           height: '50px'
+        }
+      }
+    },
+
+    components: {
+      'gallery': VueGallery
+    },
+  }
+</script>
